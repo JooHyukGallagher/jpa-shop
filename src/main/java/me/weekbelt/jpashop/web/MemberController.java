c7fd8c007e16eb4df96d198e61b7cf73bacbaa62
joohyuk <vfrvfr4207@gmail.com>
2020-02-11 16:31:01 +0900

    회원 등록 Controller & form

---
+++ src/main/java/me/weekbelt/jpashop/web/MemberController.java
@@ -0,0 +1,48 @@
+package me.weekbelt.jpashop.web;
+
+import lombok.RequiredArgsConstructor;
+import me.weekbelt.jpashop.domain.Address;
+import me.weekbelt.jpashop.domain.member.Member;
+import me.weekbelt.jpashop.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "/members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+        if (result.hasErrors()){
+            return "/members/createMemberForm";
+        }
+
+        Address address = Address.builder()
+                .city(form.getCity())
+                .street(form.getStreet())
+                .zipcode(form.getZipcode())
+                .build();
+
+        Member member = Member.builder()
+                .name(form.getName())
+                .address(address)
+                .build();
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
