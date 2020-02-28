8993c91dfeddcb2183b28d1f20a67a3a892dca88
joohyuk <vfrvfr4207@gmail.com>
2020-02-28 18:23:50 +0900

    Create /api/v5/orders API

--- src/main/java/me/weekbelt/jpashop/domain/order/OrderQueryRepository.java
+++ src/main/java/me/weekbelt/jpashop/domain/order/OrderQueryRepository.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -42,5 +45,35 @@ public class OrderQueryRepository {
                         " WHERE oi.order.id = :orderId", OrderItemQueryDto.class)
                 .setParameter("orderId", orderId)
                 .getResultList();
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(toOrderIds(result));
+
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+
+        return result;
+    }
+
+    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+        return result.stream()
+                .map(OrderQueryDto::getOrderId)
+                .collect(Collectors.toList());
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds){
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                "SELECT new me.weekbelt.jpashop.web.dto.order.OrderItemQueryDto(" +
+                        "oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                        " FROM OrderItem oi" +
+                        " JOIN oi.item i" +
+                        " WHERE oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        return orderItems.stream()
+                .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
     }
 }
